325c038e98b747be24d16f422890efd36d194ccb
Amarjit Singh <b.singh.amarjit@gmail.com>
2018-11-28 02:07:11 -0800

    added ui, buttons, motion, vibrate
    All main code for app to work like described

--- magic8Ball/ViewController.swift
+++ magic8Ball/ViewController.swift
@@ -7,14 +7,33 @@
 //
 
 import UIKit
+import AudioToolbox
 
 class ViewController: UIViewController {
+    
+    let magicBallArray = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+    var ballIndex = 0
 
+    @IBOutlet weak var magicBall: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func askButton(_ sender: UIButton) {
+        updateMagicBall()
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        updateMagicBall()
+    }
+    
+    func updateMagicBall(){
+        ballIndex = Int.random(in: 0 ... 4)
+        magicBall.image = UIImage.init(named: magicBallArray[ballIndex])
+        AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
+    }
+    
 }
 
